acd4b2fba05b816afb0f3e1c71dfa5d06872c734
Alberto Aguzzi <a.aguzzi@appsandbeyond.nl>
2022-02-28 16:30:14 +0100

    Finished homework part II

--- src/Week04/Homework.hs
+++ src/Week04/Homework.hs
@@ -11,7 +11,7 @@ module Week04.Homework where
 import Control.Monad.Freer.Extras as Extras
 import Data.Aeson                 (FromJSON, ToJSON)
 import Data.Functor               (void)
-import Data.Text                  (Text)
+import Data.Text                  (Text, unpack)
 import GHC.Generics               (Generic)
 import Ledger
 import Ledger.Ada                 as Ada
@@ -31,7 +31,7 @@ payContract :: Contract () PaySchema Text ()
 payContract = do
     pp <- awaitPromise $ endpoint @"pay" return
     let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
-    void $ submitTx tx
+    handleError (\err -> Contract.logError $ "caught: " ++ unpack err) $ void $ submitTx tx
     payContract
 
 -- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
@@ -39,15 +39,15 @@ payContract = do
 -- after each endpoint call.
 payTrace :: Integer -> Integer -> EmulatorTrace ()
 payTrace x y = do
-    h1 <- activateContractWallet (knownWallet 1) payContract
-    callEndpoint @"pay" h1 $ PayParams
+    h <- activateContractWallet (knownWallet 1) payContract
+    callEndpoint @"pay" h $ PayParams
         { ppRecipient = mockWalletPaymentPubKeyHash $ knownWallet 2
-        , ppLovelace      = x
+        , ppLovelace  = x
         }
     void $ Emulator.waitNSlots 1
-    callEndpoint @"pay" h1 $ PayParams
+    callEndpoint @"pay" h $ PayParams
         { ppRecipient = mockWalletPaymentPubKeyHash $ knownWallet 2
-        , ppLovelace      = y
+        , ppLovelace  = y
         }
     s <- Emulator.waitNSlots 1
     Extras.logInfo $ "reached " ++ show s
@@ -56,4 +56,4 @@ payTest1 :: IO ()
 payTest1 = runEmulatorTraceIO $ payTrace 10_000_000 20_000_000
 
 payTest2 :: IO ()
-payTest2 = runEmulatorTraceIO $ payTrace 1000_000_000 20_000_000
+payTest2 = runEmulatorTraceIO $ payTrace 20_000_000 1000_000_000
